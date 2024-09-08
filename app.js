c4acb4d6c576f397509b5bed2315f570adf24150
rajorshi-mandal <raj86969051@gmail.com>
2024-09-08 13:00:12 +0530

    WanderLust Phase 3 c Completed and WanderLust pHase 3 d minor changes remainng (not sure completed early for code part), for part c Added Search Bar in the website , with fitler icons like Trending, Mountains, Rooms, etc fixed the home page by resetting /home to /listings, added a toggle checkbox for GST included price to be disaplyed on lsitings

--- app.js
+++ app.js
@@ -10,15 +10,31 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
+const dbURL = process.env.ATLASDB_URL;
+
 const port = 8080;
 
+const store = MongoStore.create({
+    mongoUrl: dbURL,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+    console.log("ERROR in MONGO SESSION STORE", err);
+})
+
 const sessionOptions = {
-    secret: "mysupersecretscode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -28,9 +44,9 @@ const sessionOptions = {
     },
 };
 
-app.get("/", (req,res) => {
-    res.send("Hi, I am root");
-})
+// app.get("/", (req,res) => {
+//     res.send("Hi, I am root");
+// })
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -48,9 +64,10 @@ const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+// const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbURL);
 }
 
 main().then(() => {
